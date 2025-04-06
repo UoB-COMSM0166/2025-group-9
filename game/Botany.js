1a3d7674d8549926c633e22075f8ceb098b72512
satvikamal <satvika.mallela@gmail.com>
2025-04-06 23:55:38 +0100

    Player classes

---
+++ game/Botany.js
@@ -0,0 +1,33 @@
+class Botany{ // add extends Sprite
+  constructor(){
+    this.position = { 
+      x: 100, 
+      y: 100 
+    }
+    this.velocity = { x: 0, y: 0 }
+    this.width = 100
+    this.height = 100
+    this.sides = {
+      bottom: this.position.y + this.height
+    }
+    this.gravity = 1;
+  }
+    
+    draw(){
+      // for testing purposes only -- remove later
+      c.fillStyle = 'blue';
+      c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+   update(){
+    this.position.x += this.velocity.x;  
+    this.position.y += this.velocity.y;
+      this.sides.bottom = this.position.y + this.height;
+      // above the bottom of canvas
+      if (this.sides.bottom + this.velocity.y < canvas.height){
+        this.velocity.y += this.gravity;
+        //this.velocity.y = this.velocity + this.gravity;
+      } else this.velocity.y = 0;
+    }
+} 
+  //console.log('leaving the class');
