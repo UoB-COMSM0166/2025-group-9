be742dde718ef9528d6d4a78f2f186100714354c
ahl-hx <lamptey.hakeem@outlook.com>
2025-04-12 15:16:03 +0100

    collision adjustment

--- game/sketch.js
+++ game/sketch.js
@@ -59,51 +59,51 @@ function loadHardPlatforms() {
   platforms = [];
 
   //ground floor 
-  platforms.push({ x: 218 - 147 + xOffset, y: 750 - 32 + yOffset, width: 1298, height: 50 });
-  platforms.push({ x: 218 - 147 + xOffset, y: 750 - 32 - 27 + yOffset, width: 421, height: 27 });
-  platforms.push({ x: 218 - 147 + xOffset, y: 750 - 32 - 27 - 27 + yOffset, width: 373, height: 27 });
+  platforms.push({ x: 71 + xOffset, y: 718 + yOffset, width: 1298, height: 50 });
+  platforms.push({ x: 71 + xOffset, y: 691 + yOffset, width: 421, height: 27 });
+  platforms.push({ x: 71 + xOffset, y: 664 + yOffset, width: 373, height: 27 });
 
   //table
-  platforms.push({ x: 1153 + 30 + 41 + xOffset, y: 712 - 27 + yOffset, width: 50, height: 5 });
+  platforms.push({ x: 1224 + xOffset, y: 685 + yOffset, width: 50, height: 5 });
 
 
   //first floor
-  platforms.push({ x: 218 - 147 + xOffset, y: 500 - 7 + yOffset, width: 801, height: 54 });
-  platforms.push({ x: 959 + 24 + xOffset, y: 501 - 7 + yOffset, width: 386, height: 54 });
+  platforms.push({ x: 71 + xOffset, y: 493 + yOffset, width: 801, height: 54 });
+  platforms.push({ x: 983 + xOffset, y: 494 + yOffset, width: 386, height: 54 });
 
   //additional first floor platforms
-  platforms.push({ x: 552 - 72 + xOffset, y: 500 - 7 - 26 + yOffset, width: 240, height: 26 });
-  platforms.push({ x: 572 - 70 + xOffset, y: 500 - 7 - 26 - 18 + yOffset, width: 193, height: 18 });
+  platforms.push({ x: 480 + xOffset, y: 467 + yOffset, width: 240, height: 26 });
+  platforms.push({ x: 502 + xOffset, y: 449 + yOffset, width: 193, height: 18 });
   //platforms.push({ x: 783, y: 411, width: 55, height: 17 });
-  platforms.push({ x: 993 + 14  + xOffset, y: 403 + 37 + yOffset, width: 48, height: 19 });
-  platforms.push({ x: 1070 + 12 + xOffset, y: 413 + 1 + yOffset, width: 48, height: 19 });
-  platforms.push({ x: 218 - 147 + 58 + xOffset, y: 500 - 7 - 52 + yOffset, width: 52, height: 52 });
-  platforms.push({ x: 1197 + 81 + xOffset, y: 501 - 7 - 49 + yOffset, width: 49, height: 49 });
+  platforms.push({ x: 1007  + xOffset, y: 440 + yOffset, width: 48, height: 19 });
+  platforms.push({ x: 1082 + xOffset, y: 414 + yOffset, width: 48, height: 19 });
+  platforms.push({ x: 129 + xOffset, y: 441 + yOffset, width: 52, height: 52 });
+  platforms.push({ x: 1278 + xOffset, y: 445 + yOffset, width: 49, height: 49 });
 
 
   //second floor
-  platforms.push({ x: 218 - 147 + xOffset, y: 252 + 18 + yOffset, width: 806, height: 54 });
-  platforms.push({ x: 960 + 26 + xOffset, y: 252 + 19 + yOffset, width: 384, height: 54 });
+  platforms.push({ x: 71 + xOffset, y: 270 + yOffset, width: 806, height: 54 });
+  platforms.push({ x: 986 + xOffset, y: 271 + yOffset, width: 384, height: 54 });
 
   //additional second floor platforms
-  platforms.push({ x: 402 - 107 + xOffset, y: 222 + 22 + yOffset, width: 158, height: 27 });
-  platforms.push({ x: 218 - 147 + xOffset, y: 192 - 26 + 52 + yOffset, width: 110, height: 52 });
-  platforms.push({ x: 1298 + 218 - 107 - 147 + xOffset, y: 192 - 26 + 52 + yOffset, width: 107, height: 53 });
-  platforms.push({ x: 1097 + 60 + xOffset, y: 202 + 24 + yOffset, width: 39 + 7, height: 18 });
-  platforms.push({ x: 1048 + 49 + xOffset, y: 175 + 26 + yOffset, width: 39 + 8, height: 18 });
-  platforms.push({ x: 999 + 38 + xOffset, y: 143 + 87 + yOffset, width: 39 + 8, height: 18 });
-  platforms.push({ x: 340 - 117 + xOffset, y: 167 + 58 + yOffset, width: 30 + 9, height: 19 });
-  platforms.push({ x: 432 + 54 + xOffset, y: 122 + 103 + yOffset, width: 30 + 9, height: 19 });
-  platforms.push({ x: 218 - 147 + 262 + xOffset, y: 153 + yOffset, width: 30 + 7, height: 19 });
-  platforms.push({ x: 659 - 47 + xOffset, y: 162 + 27 + yOffset, width: 122, height: 18 });
+  platforms.push({ x: 295 + xOffset, y: 244 + yOffset, width: 158, height: 27 });
+  platforms.push({ x: 71 + xOffset, y: 218 + yOffset, width: 110, height: 52 });
+  platforms.push({ x: 1262 + xOffset, y: 218 + yOffset, width: 107, height: 53 });
+  platforms.push({ x: 1157 + xOffset, y: 226 + yOffset, width: 46, height: 18 });
+  platforms.push({ x: 1097 + xOffset, y: 201 + yOffset, width: 47, height: 18 });
+  platforms.push({ x: 1037 + xOffset, y: 230 + yOffset, width: 47, height: 18 });
+  platforms.push({ x: 223 + xOffset, y: 225 + yOffset, width: 39, height: 19 });
+  platforms.push({ x: 486 + xOffset, y: 225 + yOffset, width: 39, height: 19 });
+  platforms.push({ x: 333 + xOffset, y: 153 + yOffset, width: 37, height: 19 });
+  platforms.push({ x: 612 + xOffset, y: 189 + yOffset, width: 122, height: 18 });
 
 
   //building boundaries
-  platforms.push({ x: 218 - 147 + xOffset, y: 44 + yOffset, width: 1298, height: 54 });
-  platforms.push({ x: 213 - 147 + xOffset, y: 0 + yOffset, width: 5, height: 820 });
-  platforms.push({ x: 1272 + 147 - 51 + xOffset, y: 0 + yOffset, width: 5, height: 820 });
+  platforms.push({ x: 71 + xOffset, y: 44 + yOffset, width: 1298, height: 54 });
+  platforms.push({ x: 66 + xOffset, y: 0 + yOffset, width: 5, height: 820 });
+  platforms.push({ x: 1368 + xOffset, y: 0 + yOffset, width: 5, height: 820 });
 
-  lift = new Lift(218 - 147 + 803 + xOffset, 750 - 32 + 5 + yOffset, 111, 5, 2, 260 + yOffset, 750 - 32 + 6 + yOffset); // hard level lift
+  lift = new Lift(874 + xOffset, 723 + yOffset, 111, 5, 2, 260 + yOffset, 724 + yOffset); // hard level lift
 
 }
 
@@ -111,55 +111,55 @@ function loadEasyPlatforms() {
   platforms = [];
 
   //ground floor 
-  platforms.push({ x: 218 - 147 + xOffset, y: 750 - 32 + 3 + yOffset, width: 1298, height: 50 });
+  platforms.push({ x: 71 + xOffset, y: 719 + yOffset, width: 1298, height: 50 });
 
 
   //additional ground floor
-  platforms.push({ x: 1097 + 60 - 23 - 596 + xOffset, y: 202 + 24 - 50 + 2 + 1 + 505 + yOffset, width: 66, height: 16 });
-  platforms.push({ x: 1097 + 60 - 23 - 596 - 66 + 2 + xOffset, y: 202 + 24 - 50 + 2 + 1 + 506 - 44 + yOffset, width: 66, height: 16 });
-  platforms.push({ x: 1097 + 60 - 23 - 596 - 66 - 196 + 1 + xOffset, y: 202 + 24 - 50 + 2 + 1 + 506 - 44 + yOffset, width: 66, height: 16 });
-  platforms.push({ x: 1097 + 60 - 23 - 596 - 333 + xOffset, y: 202 + 24 - 50 + 2 + 1 + 503 - 1 + yOffset, width: 66, height: 16 });
-  platforms.push({ x: 1298 + 218 - 107 - 147 + 2 - 219 - 734 + 306 - 247 + xOffset, y:  500 - 7 + 15  - 53 - 44 + 267 + yOffset, width: 75, height: 43 });
+  platforms.push({ x: 537 + xOffset, y: 682 + yOffset, width: 67, height: 17 });
+  platforms.push({ x: 473 + xOffset, y: 640 + yOffset, width: 67, height: 18 });
+  platforms.push({ x: 277 + xOffset, y: 639 + yOffset, width: 66, height: 16 });
+  platforms.push({ x: 204 + xOffset, y: 680 + yOffset, width: 66, height: 16 });
+  platforms.push({ x: 370 + xOffset, y: 678 + yOffset, width: 75, height: 43 });
 
 
   //first floor
-  platforms.push({ x: 218 - 147 + xOffset, y: 500 - 7 + 15 + yOffset, width: 801, height: 54 });
-  platforms.push({ x: 959 + 24 + 7 + xOffset, y: 500 - 7 + 15 + yOffset, width: 386 - 7, height: 54 });
+  platforms.push({ x: 71 + xOffset, y: 506 + yOffset, width: 801, height: 54 });
+  platforms.push({ x: 990 + xOffset, y: 506 + yOffset, width: 379, height: 54 });
 
   //additional first floor platforms
-  platforms.push({ x: 1298 + 218 - 107 - 147 + 1 + xOffset, y:  500 - 7 + 15  - 53 + yOffset, width: 106, height: 53 });
-  platforms.push({ x: 1298 + 218 - 107 - 147 + 2 - 219 + xOffset, y:  500 - 7 + 15  - 53 + yOffset, width: 109, height: 53 });
-  platforms.push({ x: 1298 + 218 - 107 - 147 + 2 - 219 - 734 + xOffset, y:  500 - 7 + 15  - 53 + yOffset, width: 109, height: 53 });
-  platforms.push({ x: 1298 + 218 - 107 - 147 + 2 - 219 - 734 + 306 + xOffset, y:  500 - 7 + 15  - 53 - 44+ yOffset, width: 75, height: 50 });
-  platforms.push({ x: 218 - 147 - 3 + xOffset, y: 500 - 7 + 15 - 52 + yOffset, width: 113, height: 52 });
+  platforms.push({ x: 1263 + xOffset, y: 454 + yOffset, width: 106, height: 53 });
+  platforms.push({ x: 1045 + xOffset, y: 454 + yOffset, width: 109, height: 53 });
+  platforms.push({ x: 311 + xOffset, y: 454 + yOffset, width: 109, height: 53 });
+  platforms.push({ x: 616 + xOffset, y: 411 + yOffset, width: 75, height: 50 });
+  platforms.push({ x: 68 + xOffset, y: 455 + yOffset, width: 113, height: 52 });
 
-  platforms.push({ x: 1097 + 60 - 23 - 344 + xOffset, y: 202 + 24 - 50 + 2 + 1 + 264 - 1 + yOffset, width: 68, height: 16 });
-  platforms.push({ x: 1097 + 60 - 23 - 344 - 70 + xOffset, y: 202 + 24 - 50 + 2 + 1 + 264 - 46 + yOffset, width: 68, height: 16 });
-  platforms.push({ x: 1097 + 60 - 23 - 344 - 70 - 204 + xOffset, y: 202 + 24 - 50 + 2 + 1 + 264 - 46 + 5 + yOffset, width: 68, height: 16 });
-  platforms.push({ x: 1097 + 60 - 23 - 344 - 345 + xOffset, y: 202 + 24 - 50 + 2 + 1 + 264 + yOffset, width: 68, height: 16 });
+  platforms.push({ x: 790 + xOffset, y: 441 + yOffset, width: 68, height: 16 });
+  platforms.push({ x: 720 + xOffset, y: 396 + yOffset, width: 68, height: 16 });
+  platforms.push({ x: 516 + xOffset, y: 401 + yOffset, width: 68, height: 16 });
+  platforms.push({ x: 445 + xOffset, y: 441 + yOffset, width: 68, height: 16 });
   
   
   //second floor
-  platforms.push({ x: 218 - 147 + xOffset, y: 252 + 18 + 5 + yOffset, width: 797, height: 54 });
-  platforms.push({ x: 960 + 26 + xOffset, y: 252 + 19 + 10 + yOffset, width: 384, height: 54 });
+  platforms.push({ x: 71 + xOffset, y: 273 + yOffset, width: 797, height: 54 });
+  platforms.push({ x: 986 + xOffset, y: 279 + yOffset, width: 384, height: 54 });
 
   //additional second floor platforms
-  platforms.push({ x: 402 - 107 + 10 + xOffset, y: 252 + 18 + 5 - 52 + yOffset, width: 202, height: 52 });
-  platforms.push({ x: 218 - 147 - 3 + xOffset, y: 252 + 18 + 5 - 52 + yOffset, width: 110, height: 52 });
-  platforms.push({ x: 402 - 107 + 10 + 391 + xOffset, y: 252 + 18 + 5 - 52 + yOffset, width: 172, height: 52 });
-  platforms.push({ x: 1298 + 218 - 107 - 147 + 2 + xOffset, y: 252 + 19 + 10 - 53 + yOffset, width: 105, height: 53 });
-  platforms.push({ x: 1298 + 218 - 107 - 147 + 2 - 278 + xOffset, y: 252 + 19 + 10 - 53 + yOffset, width: 158, height: 53 });
-  platforms.push({ x: 1097 + 60 - 23 + xOffset, y: 202 + 24 - 50 + 2 + 1 + yOffset, width: 68, height: 16 });
-  platforms.push({ x: 340 - 113 - 5 - 4 + xOffset, y: 167 + 58 - 30 + 3 + 2 + yOffset, width: 46, height: 47 });
+  platforms.push({ x: 305 + xOffset, y: 222 + yOffset, width: 202, height: 52 });
+  platforms.push({ x: 68 + xOffset, y: 222 + yOffset, width: 110, height: 52 });
+  platforms.push({ x: 696 + xOffset, y: 222 + yOffset, width: 172, height: 52 });
+  platforms.push({ x: 1264 + xOffset, y: 227 + yOffset, width: 105, height: 53 });
+  platforms.push({ x: 986 + xOffset, y: 227 + yOffset, width: 158, height: 53 });
+  platforms.push({ x: 1134 + xOffset, y: 178 + yOffset, width: 68, height: 16 });
+  platforms.push({ x: 218 + xOffset, y: 199 + yOffset, width: 46, height: 48 });
 
 
 
   //building boundaries
-  platforms.push({ x: 218 - 147 + xOffset, y: 44 + yOffset, width: 1298, height: 54 });
-  platforms.push({ x: 213 - 147 + xOffset, y: 0 + yOffset, width: 5, height: 820 });
-  platforms.push({ x: 1272 + 147 - 51 + xOffset, y: 0 + yOffset, width: 5, height: 820 });
+  platforms.push({ x: 71 + xOffset, y: 44 + yOffset, width: 1298, height: 54 });
+  platforms.push({ x: 66 + xOffset, y: 0 + yOffset, width: 5, height: 820 });
+  platforms.push({ x: 1368 + xOffset, y: 0 + yOffset, width: 5, height: 820 });
 
-  lift = new Lift(218 - 147 + 803 + xOffset, 750 - 32 + 5 + yOffset, 111, 5, 2, 260 - 85 + yOffset, 750 - 32 + 6 + yOffset); // easy level lift
+  lift = new Lift(874 + xOffset, 723 + yOffset, 111, 5, 2, 175 + yOffset, 724 + yOffset); // easy level lift
 
 }
 
